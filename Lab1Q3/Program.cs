130121e895ac4573d119f164a87d2abe088276a4
IPK Computers <IPK Computers@DESKTOP-LFV0JB5>
2024-01-25 17:42:39 +0530

    Q3

---
+++ Lab1Q3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0;
+
+            Console.Write("Enter a positive number:");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                for (int i = 1; 1 < +number; i++)
+                {
+                    sum = sum + 1;
+                    //sum+=1
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
